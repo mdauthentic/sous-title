59351ee200baead8d3a53c33b2f36b45116218d4
mlawal <muideen.lawal@inria.fr>
2020-05-08 00:31:16 +0200

    file extension method added

--- src/main/scala/io/github/mdauthentic/core/Utils.scala
+++ src/main/scala/io/github/mdauthentic/core/Utils.scala
@@ -12,10 +12,19 @@ trait Utils {
    * @param n: length of character to be created
    * */
   def setHashDelim(n: Int = n): String = setHashDelim * n
+
   /**
    * create repeated `*`
    * @param n: length of character to be created
    * */
   def setStarDelim(n: Int = n): String = setStarDelim * n
+
+  /**
+   * Get file extension
+   *
+   * @param filename: full file path
+   * */
+  def getFileExtension(filename: String): Option[String] =
+    Some(filename).filter(_.contains(".")).map(_.split("\\.").last)
 
 }
